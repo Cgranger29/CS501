b9e9551c3bdddd0a370f86cc15491dac3021409f
cgranger29 <cgranger29@gmail.com>
2022-02-03 19:51:48 -0500

    adding incomplete password script

---
+++ password_mod/src/Password.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Password{
+    public static void main(String[] args){
+
+        String inputPassword;
+        String newPassword;
+
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Please enter a password ");
+    
+        inputPassword = scnr.next();
+        
+        for(int i = 0; i < inputPassword.length(); i++){
+            char currChar = inputPassword.charAt((i));
+            currChar = (String)currChar;
+            System.out.println(currChar);
+            // System.out.println(currChar.getClass().getName());
+            // inputPassword.replace(currChar, "a");
+        }
+        System.out.println(inputPassword.getClass().getName());
+        scnr.close();
+
+    }
+}
